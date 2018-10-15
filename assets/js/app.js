9fb49d7d6bc78e61141a7d7dc3705da901a27cb4
Trey Ellis <illusiongrouppcs@gmail.com>
2018-10-14 21:20:47 -0400

    Setting Base Framework: 	-Installed Symfony Annotations Bundle for easy routes 	-Installed Symfony Webpack Encore for handling css, scss, and js (high optimization) 	-Installed latest version of JQuery and Popper js 	-Installed latest version of Bootstrap js and scss 	-Created global.scss to manipulate bootstrap.scss variables (Easy editing of bootstrap without changing sources) 	-Linked created app.css and app.js to base.html.twig 	-Created HomeController and a index action for the homepage 	-Installed Symfony Asset Bundle to use {{asset('link to asset')}} for easy asset links

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+const $ = require('jquery');
+// JS is equivalent to the normal "bootstrap" package
+// no need to set this to a variable, just require it
+require('bootstrap');
+require('../scss/global.scss');
+// any CSS you require will output into a single css file (app.css in this case)
+require('../css/app.css');
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// var $ = require('jquery');
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
